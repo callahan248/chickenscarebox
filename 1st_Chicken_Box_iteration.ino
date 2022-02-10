8852e20062559f71adbd5fa64d43bd7cf3abca05
callahan248 <69098207+callahan248@users.noreply.github.com>
2022-02-09 19:33:35 -0700

    Add files via upload

--- 1st_Chicken_Box_iteration.ino
+++ 1st_Chicken_Box_iteration.ino
@@ -1,32 +1,53 @@
-const byte  ledPin = 13, pinBuzzer = 11, rPin = 10, bPin = 8, pirPin = 12; //LED=detect wehther or not it is armed or went off, Buzzer = Soundboard, R pin = Relay 1 pin, B pin =
-uint16_t    distance;
-bool        isHuman = false, update = false, armed = true,
-            ultaRead = false;
-uint8_t     eventCount = 0, rPinState = LOW, bPinState = LOW,
-            ledState = HIGH, blinkCount = 0;
+const byte  ledPin = 13, relayOne = 10, relayTwo = 8, pirPin = 12, buttonPin = 9;
+
+bool isMotion = false, update = false, armed = false, distanceRead = false;   //Come back and change "is motion" to true and see if that changes anything
+     
+uint8_t eventCount = 0, relayOneState = LOW, relayTwoState = LOW, ledState = HIGH, blinkCount = 0;
+            
+uint16_t distance;
+
 uint32_t prevTime = 0;
 
+int trigPin = 7;      //defines the trigger pin from Ultrasonic Sensor
+int echoPin = 6;      //defines the echo pin from Ultrasonic Sensor
+
+// #define MINUTE 200      //200 x DUTY = 20,000 or 20 seconds
 #define DUTY 100
-#define MINUTE 200      //200 x DUTY = 20,000 or 20 seconds
-#define SIXSEC 50       //50 x DUTY = 5000 (five seconds)
+#define SIXSEC 60       //60 x DUTY = 6000 (Six Seconds)
 #define THREE 3         //For LED blink @ 3 x DUTY or 300mS rate
 
-int buttonPin = 9; 
+int calibrationTime = 30;
+
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(7, OUTPUT);           //TRIG
-  pinMode (6, INPUT);           //ECHO
-  pinMode(pinBuzzer, OUTPUT);  //
-  pinMode(rPin, OUTPUT);        //Define start state
-  pinMode(bPin, OUTPUT);
-  pinMode(pirPin, INPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-  digitalWrite(rPin, rPinState);
-  digitalWrite(bPin, bPinState);
-  digitalWrite(ledPin, ledState);
-}
+  //Setup pins other than the Ultrasonic Sensor
+  pinMode(ledPin, OUTPUT);                //Sets LED as output
+  pinMode(relayOne, OUTPUT);              //sets relay one (Soundboard) as output
+  pinMode(relayTwo, OUTPUT);              //sets relay two (Pneumatics) as output
+  pinMode(buttonPin, INPUT_PULLUP);       // Uses onboard pullup resistor as button input
+  digitalWrite(relayOne, relayOneState);  //writes relayOne to "LOW" as stated in the init.
+  digitalWrite(relayTwo, relayTwoState);  //writes relayTwo to "LOW" as stated in the init.
+  digitalWrite(ledPin, ledState);         //writes LED to "LOW" as stated in the init.
 
+  pinMode(pirPin, INPUT);                 //sets Motion Sensor as input
+
+  Serial.begin (9600);
+
+
+  Serial.print("calibrating sensor "); //give the sensor some time to calibrate
+  for (int i = 0; i < calibrationTime; i++) { //will count to 30 seconds before sensor is active
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println(" done");
+  Serial.println("SENSOR ACTIVE");
+  delay(50);
+//Start Calibration of Ultrasonic Sensor
+pinMode (trigPin, OUTPUT);               //Sets TRIGGER pin as output
+pinMode (echoPin, INPUT);                //Sets ECHO pin as input
+delay (1000);
+}
+//command for ititial ultrasonic sensor reading
 uint32_t duration(uint8_t triggerPin, uint8_t echoPin) {
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
@@ -34,63 +55,117 @@ uint32_t duration(uint8_t triggerPin, uint8_t echoPin) {
   delayMicroseconds(20);
   digitalWrite(triggerPin, LOW);
   return pulseIn(echoPin, HIGH);
+  Serial.println("Distance Calibrated");
+  delay (1000); 
+  Serial.println("System is armed");
 }
 
 void loop() {
 
-
   if (armed) {
-    isHuman = digitalRead(pirPin);
-    if (isHuman) {
-      ultaRead = true;
+    isMotion = digitalRead(pirPin);
+    if (isMotion) {
+      distanceRead = true;
     }
   }
-
   if (millis() - prevTime >= DUTY) {
     prevTime = millis();
-    if (ultaRead) {
-      //Distance is in "mm" and it's rounded up/down to nearest "mm"
-      distance = ((float)((0.01723 * duration(7, 6)) * 10) + 0.5);
-      //Take a single distance read to see if object is 2000mm() or closer
-      if (distance <= 2000) {
+    if (distanceRead) {   //Distance is in "mm" and it's rounded up/down to nearest "mm"
+         distance = ((float)((0.01723 * duration(trigPin, echoPin)) * 10) + 0.5); //Take a single distance read to see if object is 2000mm() or closer
+
+      if (distance <= 2000) {   // if distance is less than or = to 2000 ()
         armed = false;        //Disarm sensors and enable events
         eventCount = 0;       //Zero event timer counter
         blinkCount = 0;       //Zero blink timer counter
-        //                        tone(pinBuzzer);  //Buzzer on
-        rPinState = HIGH;     //Relay 1 on
+        relayOneState = HIGH;     //Relay 1 on            // This might be the issue*****
         update = true;        //Update Outputs
       }
-      ultaRead = false;         //Cancel UltraSonic read after each sample
+      distanceRead = false;         //Cancel UltraSonic read after each sample
     }
 
-    if (!armed) {                     //Events enabled, Sensors disabled
+    if (!armed) {                     //If NOT ARMED (Activated) ...Events enabled, Sensors disabled
       eventCount++;                   //Plus 1 each duty cycle
       blinkCount++;
       if (blinkCount >= THREE) {  //Blink LED every 300mS
-        digitalWrite(ledPin, (ledState = !ledState));
+        digitalWrite(ledPin, (ledState = !ledState)); //
         blinkCount = 0;
         if (digitalRead(buttonPin) == LOW) {
-          armed = true; 
-        }                                     // IF Button is pressed, armed = true
+          armed = true;                       // IF Button is pressed, armed = true (this will be how we reset it)
+        }                                     
       }
-      if ((eventCount == MINUTE) && (armed == true)) { //25,000mS Arm sensors and LED ON solid
-
+      if (armed == true) { // If armed = true, Arm both sensors and turn LED ON solid
         digitalWrite(ledPin, HIGH);
-      } else if (eventCount == SIXSEC) { //6000mS Buzzer and 2 x relays are turned off
-        noTone(pinBuzzer);
-        bPinState = LOW;
-        rPinState = LOW;
+      } 
+      else if (eventCount == SIXSEC) { //6000mS Buzzer and 2 x relays are turned off
+        relayTwoState = LOW;
+        relayOneState = LOW;
         update = true;
-      } else if (eventCount == THREE) {  //300mS delay for On state of bPin
-        bPinState = HIGH;
+      } else if (eventCount == THREE) {  //300mS delay for On state of relayTwo
+        relayTwoState = HIGH;    //here
         update = true;
       }
     }
 
     if (update) {                     //So we only write to the ouputs when needed
-      digitalWrite(rPin, rPinState);
-      digitalWrite(bPin, bPinState);
+      digitalWrite(relayOne, relayOneState);
+      digitalWrite(relayTwo, relayTwoState);
       update = false;
     }
   }
 }
+
+
+
+
+
+
+// Sub-routines:
+
+//
+//void systemArmed (){
+//  if (armed) {
+//    isMotion = digitalRead(pirPin);
+//    if (isMotion) {
+//      distanceRead = true;
+//    }
+//  }
+//  if (millis() - prevTime >= DUTY) {
+//    prevTime = millis();
+//    if (distanceRead) {   //Distance is in "mm" and it's rounded up/down to nearest "mm"
+//         distance = ((float)((0.01723 * duration(trigPin, echoPin)) * 10) + 0.5); //Take a single distance read to see if object is 2000mm() or closer
+//
+//      if (distance <= 2000) {   // if distance is less than or = to 2000 ()
+//        armed = false;        //Disarm sensors and enable events
+//        eventCount = 0;       //Zero event timer counter
+//        blinkCount = 0;       //Zero blink timer counter
+//        relayOneState = HIGH;     //Relay 1 on            // This might be the issue*****
+//        update = true;        //Update Outputs
+//      }
+//      distanceRead = false;         //Cancel UltraSonic read after each sample
+//    }
+//  }
+
+//void systemDisarmed () {
+//    if (!armed) {                     //If NOT ARMED (Activated) ...Events enabled, Sensors disabled
+//      eventCount++;                   //Plus 1 each duty cycle
+//      blinkCount++;
+//      if (blinkCount >= THREE) {  //Blink LED every 300mS
+//        digitalWrite(ledPin, (ledState = !ledState)); //
+//        blinkCount = 0;
+//        if (digitalRead(buttonPin) == LOW) {
+//          armed = true;                       // IF Button is pressed, armed = true (this will be how we reset it)
+//        }                                     
+//      }
+//      if (armed == true) { // If armed = true, Arm both sensors and turn LED ON solid
+//        digitalWrite(ledPin, HIGH);
+//      } 
+//      else if (eventCount == SIXSEC) { //6000mS Buzzer and 2 x relays are turned off
+//        relayTwoState = LOW;
+//        relayOneState = LOW;
+//        update = true;
+//      } else if (eventCount == THREE) {  //300mS delay for On state of relayTwo
+//        relayTwoState = HIGH;
+//        update = true;
+//      }
+//    }
+//}
